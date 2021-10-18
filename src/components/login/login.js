f87dac4091afc3007237d2b0f7c74ea581a73ef7
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-10-18 13:47:58 +0530

    Create functions to handle actions

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -14,6 +14,33 @@ function Logo() {
 
 
 class InputBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {username: '', password: ''}
+        this.entryChange = this.onEntryChange.bind(this)
+        this.onLogin = this.onSubmit.bind(this)
+        this.newUser = this.onNewUser.bind(this)
+    }
+
+    onEntryChange(event) {
+        if(event.target.id === 'username') {
+            this.setState({username: event.target.value})
+        }
+        if(event.target.id === 'password') {
+            this.setState({password: event.target.value})
+        }
+    }
+
+    onSubmit() {
+        console.log('Submit action detected')
+        console.log('username: '+this.state.username)
+        console.log('password: '+this.state.password)
+    }
+
+    onNewUser() {
+        console.log('New user request detected')
+    }
+
     render() {
         return (
             <div className='entry-container'>
@@ -22,17 +49,19 @@ class InputBox extends React.Component {
                 <TextField
                 id='username'
                 label='Username'
+                onChange={this.entryChange}
                 variant='outlined' />
                 
                 <TextField
                 id='password'
                 label='Password'
+                onChange={this.entryChange}
                 type='password'
                 variant='outlined' />
 
                 <div className='button-container'>
-                    <Button variant='text'>Login</Button>
-                    <Button variant='text'>New user</Button>
+                    <Button variant='text' onClick={this.onLogin}>Login</Button>
+                    <Button variant='text' onClick={this.newUser}>New user</Button>
                 </div>
             </div>
         );
