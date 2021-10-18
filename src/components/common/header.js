be0574b22563de50bacff96888ac921a66c573fa
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-10-18 21:10:46 +0530

    Complete header

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,9 +1,82 @@
+import React from 'react';
 import './header.css';
-import { green } from '@mui/material/colors'
+import { green } from '@mui/material/colors';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { ListItemIcon, Divider } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import ListIcon from '@mui/icons-material/List';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import EditIcon from '@mui/icons-material/Edit';
+import GroupsIcon from '@mui/icons-material/Groups';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+
+function viewProfile() {
+    console.log('Clicked View profile')
+}
+
+function editProfile() {
+    console.log('Clicked Edit profile')
+}
+
+function myGroups() {
+    console.log('Clicked My groups')
+}
+
+function searchUsers() {
+    console.log('Clicked Search users')
+}
+
+function onLogout() {
+    console.log('Clicked Logout')
+}
+
+
+function Options(props) {
+    return (
+        <Menu
+        anchorEl={props.anchorEl}
+        open={props.open}
+        onClose={props.onClose}
+        onClick={props.onClick}
+        PaperProps={props.PaperProps}
+        transformOrigin={props.transformOrigin}
+        anchorOrigin={props.anchorOrigin} >
+            <MenuItem onClick={viewProfile}>
+                <ListItemIcon>
+                    <AccountCircleIcon />
+                </ListItemIcon>
+                Profile
+            </MenuItem>
+            <MenuItem onClick={editProfile}>
+                <ListItemIcon>
+                    <EditIcon />
+                </ListItemIcon>
+                Edit profile
+            </MenuItem>
+            <MenuItem onClick={myGroups}>
+                <ListItemIcon>
+                    <GroupsIcon />
+                </ListItemIcon>
+                My Groups
+            </MenuItem>
+            <MenuItem onClick={searchUsers}>
+                <ListItemIcon>
+                    <PersonSearchIcon />
+                </ListItemIcon>
+                Search users
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={onLogout}>
+                <ListItemIcon>
+                    <LogoutIcon />
+                </ListItemIcon>
+                Logout
+            </MenuItem>
+        </Menu>
+    );
+}
 
 
 function Logo() {
@@ -15,16 +88,32 @@ function Logo() {
 }
 
 
-export default function Header() {
+export default function Header(props) {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event) => { setAnchorEl(event.currentTarget) }
+    const handleClose = () => { setAnchorEl(null) }
+    const loggedin = props.loggedin
+
     return (
+        <>
         <div className='header-container'>
             <Logo />
-            <IconButton
+            {loggedin && <IconButton
+            onClick={handleClick}
             sx={{ color: green[50] }} >
                 <AccountCircleIcon
                 fontSize='large'
                 sx={{ color: green[300] }} />
-            </IconButton>
+            </IconButton>}
         </div>
+        {loggedin && <Options
+        anchorEl={anchorEl}
+        open={open}
+        onClick={handleClose}
+        onClose={handleClose}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} />}
+        </>
     );
 }
