764c67aea6f721fe86cb1dc1e3eac5e4e61d48fd
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-10-19 22:08:56 +0530

    Minor change

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import './login.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
