b62a983a4c1e036b915c325c422938e9e0f21512
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-10-19 21:37:36 +0530

    Integrate pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Login from "./components/login/login"
-import Footer from "./components/common/footer"
-import Header from "./components/common/header"
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Login from './components/login/login';
+import NewUser from './components/newuser/newuser';
+import Footer from './components/common/footer';
+import Header from './components/common/header';
 import './App.css';
 
 function App() {
+  const loggedin = false;
   return (
-    <div className="App">
-      <Header />
-      <hr />
-      <Footer loggedin={false} />
-    </div>
+    <Router className="App">
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/newuser'>
+          <Header loggedin={loggedin} />
+          <NewUser />
+        </Route>
+      </Switch>
+      <Footer loggedin={loggedin} />
+    </Router>
   );
 }
 
