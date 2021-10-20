b412530b705599ad3c38584f8b4fd7a047562349
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-10-20 11:41:32 +0530

    Remove logout button from footer

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -5,16 +5,13 @@ import Button from '@mui/material/Button'
 class Options extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {loggedin: props.loggedin}
     }
 
     render() {
-        const logout_button = <Button variant='text'>Logout</Button>;
         return (
             <div className='options-container'>
                 <Button variant='text'>About us</Button>
                 <Button variant='text'>Home</Button>
-                {this.state.loggedin && logout_button}
             </div>
         );
     }
@@ -24,13 +21,12 @@ class Options extends React.Component {
 export default class Footer extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {loggedin: props.loggedin}
     }
 
     render() {
         return (
             <div className='footer-container'>
-                <Options loggedin={this.state.loggedin} />
+                <Options />
             </div>
         );
     }
